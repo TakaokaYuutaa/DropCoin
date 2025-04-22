190cf685fe89e2117e873a25ac60e00964bbcd6f
Taokata_yu <dorasuke5897@gmail.com>
2025-04-22 15:33:58 +0900

    フォント追加

--- Assets/Scripts/CoinSetiing.cs
+++ Assets/Scripts/CoinSetiing.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CoinSetiing : MonoBehaviour
 {
     [SerializeField] GameObject _coin;
-    Variable Vari;
+    public Variable Vari;
     private void Start()
     {
         for (int i = 0; i < Random.Range(15, 25); i++)
@@ -27,7 +27,7 @@ public class CoinSetiing : MonoBehaviour
     void CoinSet()
     {
         Vector3 _raPos;
-        _raPos = new Vector3(Random.Range(-2f, 2f), 0.8f, Random.Range(1.5f, 5.5f));
+        _raPos = new Vector3(Random.Range(-2f, 2f), 2f, Random.Range(1.5f, 5.5f));
         Rigidbody rb = _coin.GetComponent<Rigidbody>();
         _coin.transform.position = _raPos;
         GameObject Coin = Instantiate(_coin);
--- Assets/Scripts/Variable.cs
+++ Assets/Scripts/Variable.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Variable : MonoBehaviour
 {
-    public int coinSetNam=10;
+    [SerializeField] Text _CoinText;
+    public int coinSetNam = 10;
+    private void Update()
+    {
+        _CoinText.text = "Coin:" + coinSetNam;
+    }
 }
