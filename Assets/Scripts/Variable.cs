f01f675a8180ff9057c38e5db32687887db2afc0
Taokata_yu <dorasuke5897@gmail.com>
2025-04-22 15:45:19 +0900

    フォントコミット

--- Assets/Scripts/Variable.cs
+++ Assets/Scripts/Variable.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Variable : MonoBehaviour
 {
-    public int coinSetNam=10;
+    [SerializeField] Text _CoinsetNam;
+    public int coinSetNam = 10;
+    private void Update()
+    {
+        _CoinsetNam.text = "Coin:" + coinSetNam;
+    }
 }
